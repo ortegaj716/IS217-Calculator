c1ec460bf10c9c6a1bbe21889b2d668a5efec6ac
ortegaj716 <ortegaj716@gmail.com>
2014-03-22 19:54:21 -0400

    Added ajax stuff

--- public/javascripts/calculator.js
+++ public/javascripts/calculator.js
@@ -1,11 +1,40 @@
 //Variables
-num1 = 0;
-num2 = 0; 
+var num1 = 0;
+var num2 = 0; 
+var numString = "";
 
-exports = module.exports = {}; //Could be useful
+//Ajax stuff. Hope this works...
+function createRequestObject(){
+	var ro;
+	var browser = navigator.appName;
+	if(browser == "Microsoft Internet Explorer"){
+		ro = new ActiveXObject("Microsoft.XMLHTTP");
+	} else {
+		ro = new XMLHttpRequest();
+	}
 
-exports.check = function(value){
-
+	return ro;
 }
 
+//Our request object, http
+var http = createRequestObject();
+
+
+//Exports
+exports = module.exports = {};
+
+exports.check = function(value){
+	switch(value){
+		case "0": { numString += "0"; break; }
+		case "1": { numString += "1"; break; }
+		case "2": { numString += "2"; break; }
+		case "3": { numString += "3"; break; }
+		case "4": { numString += "4"; break; }
+		case "5"; { numString += "5"; break; }
+		case "6"; { numString += "6"; break; }
+		case "7"; { numString += "7"; break; }
+		case "8"; { numString += "8"; break; }
+		case "9"; { numString += "9"; break; }
+	}
+}
 
